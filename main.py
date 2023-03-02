3fa49c53caac50ee5b1043c773eb98b18018e94e
nikita.vinogradov <vinni.ru@gmail.com>
2023-03-03 00:48:12 +0300

    homework_completed

--- main.py
+++ main.py
@@ -4,22 +4,41 @@ import random
 import requests
 from question import Question
 
-if __name__ == "__main__":
-    response = requests.get('https://jsonkeeper.com/b/TXK5', verify=False)
-    print(response.content)
-    dict_questions = json.loads(response.content)
-    questions = []
-    for item in dict_questions:
-        current_question = Question(item["q"], item["d"],item["a"],item["d"]*10)
-        questions.append(current_question)
 
-    for question in questions:
-        print(question.text)
-        print(question.complexity)
-        print(question.correct_answer)
+def print_game_statistic(game_question_list: list[Question]) -> None:
+    total_points = 0
+    total_correct_answer = 0
+    for each in game_question_list:
+        if each.is_correct():
+            total_points += each.get_points()
+            total_correct_answer += 1
+    print(f"""
+    Вот и всё!
+    Отвечено на {total_correct_answer} вопроса из {len(game_question_list)}
+    Набрано баллов: {total_points}
+    """)
+
+
+if __name__ == "__main__":
+    # taking questions from json on http-server
+    response = requests.get('https://jsonkeeper.com/b/TXK5', verify=False)
+    questions_from_json = json.loads(response.content)
+
+    questions = []
+
+    for question_from_json in questions_from_json:
+        current_question = Question(question_from_json["q"], int(question_from_json["d"]), question_from_json["a"])
+        questions.append(current_question)
 
     random.shuffle(questions)
     for question in questions:
-        print(question.text)
-        print(question.complexity)
-        print(question.correct_answer)
+        question.build_question()
+        question.is_question_asked = True
+        question.user_answer = input('Your answer:')
+
+        if question.is_correct():
+            question.build_positive_feedback()
+        else:
+            question.build_negative_feedback()
+
+    print_game_statistic(questions)
--- question.py
+++ question.py
@@ -1,14 +1,13 @@
 class Question:
 
     def __repr__(self):
-        return f"The question is {self.text}. Complication of question is {self.complexity}. \
+        return f"The question is {self.text}. Complexity of question is {self.complexity}. \
         Correct answer is {self.correct_answer} "
 
     def __init__(self,
                  text,
                  complexity,
                  correct_answer,
-                 points,
                  is_question_asked=False,
                  user_answer=None
                  ):
@@ -17,13 +16,13 @@ class Question:
         self.correct_answer = correct_answer
         self.is_question_asked = is_question_asked
         self.user_answer = user_answer
-        self.points = points
+        self.points = self.complexity * 10
 
     def get_points(self):
         """Возвращает int, количество баллов.
         Баллы зависят от сложности: за 1 дается 10 баллов, за 5 дается 50 баллов.
         """
-        return self.complexity * 10
+        return self.points
 
     def is_correct(self):
         """Возвращает True, если ответ пользователя совпадает
@@ -31,22 +30,25 @@ class Question:
         """
         return self.correct_answer == self.user_answer
 
+    def set_question_asked(self):
+        self.is_question_asked = True
+
     def build_question(self):
         """Возвращает вопрос в понятном пользователю виде, например:
         Вопрос: What do people often call American flag?
         Сложность 4/5
         """
-        return f'Вопрос: {self.text}?\nСложность: {self.complexity}/5'
+        print(f'Вопрос: {self.text}?\nСложность: {self.complexity}/5')
 
     def build_positive_feedback(self):
         """Возвращает :
         Ответ верный, получено __ баллов
         """
         if self.is_correct():
-            return f'Ответ верный, получено {self.get_points()} баллов'
+            print(f'Ответ верный, получено {self.get_points()} баллов')
 
     def build_negative_feedback(self):
         """Возвращает :
         Ответ неверный, верный ответ __
         """
-        return f'Ответ неверный, верный ответ - {self.correct_answer}'
+        print(f'Ответ неверный, верный ответ - {self.correct_answer}')
